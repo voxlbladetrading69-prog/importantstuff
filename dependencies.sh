002e71eb46dfa0363a4563e8eee955d97b244fac
voxlbladetrading69-prog <voxlblade.trading69@gmail.com>
2026-03-01 21:35:58 +0800

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,10 @@
 pkg update && pkg upgrade -y \
 && pkg install -y python ncurses \
 && pkg reinstall -y python \
-&& termux-setup-storage
+&& termux-setup-storage \
+&& mkdir -p ~/myproject/termux \
+&& mkdir -p /storage/emulated/0/Android/data/com.roblox.clienu/files/gloop/external/Autoexecute \
+&& curl -L "https://raw.githubusercontent.com/voxlbladetrading69-prog/importantstuff/refs/heads/main/termux-side.py" \
+   -o ~/myproject/termux/termux-side.py \
+&& curl -L "https://raw.githubusercontent.com/voxlbladetrading69-prog/importantstuff/refs/heads/main/roblox-side.lua" \
+   -o /storage/emulated/0/Android/data/com.roblox.clienu/files/gloop/external/Autoexecute/roblox-side.lua \
