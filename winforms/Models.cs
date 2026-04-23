f07d23c8ddcf27394357f0bb458598d9d2e60e12
voxlbladetrading69-prog <voxlblade.trading69@gmail.com>
2026-04-24 00:08:46 +0800

    Create Models.cs

---
+++ winforms/Models.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+public class AccountEventRow
+{
+    public long DeviceId { get; set; }
+    public string HwidHash { get; set; } = "";
+    public string DeviceName { get; set; } = "";
+    public long AccountId { get; set; }
+    public string Username { get; set; } = "";
+    public DateTime EventTimeUtc { get; set; }
+    public Dictionary<string, object?> Values { get; set; } = new();
+}
